95a2fdb76b87b7fe292a74a1118ed04e54d9fa8e
aakash0495 <aakash.chopra95@gmail.com>
2024-09-16 16:25:20 -0400

    Routes and Basic Server side codde added

---
+++ Server/controllers/AuthController.js
@@ -0,0 +1,32 @@
+import User from "../models/UserModel.js";
+import jwt from "jsonwebtoken";
+
+const maxLimit = 3 * 24 * 60 * 60 * 1000;
+const createToken = (userId, email) =>{
+    return jwt.sign({email, userId}, process.env.JWT_KEY, {expiresIn:maxLimit})
+}
+
+export const signup = async (request, response, next) => {
+    try {
+        const {email, password} = request.body;
+        if(!email || !password){
+            return response.status(400).send("Email and Password is required");
+        }
+        const user = await User.create({email, password});
+        response.cookie("jwt", createToken(userId, email),{
+            maxLimit, 
+            secure: true,
+            sameSite: nodemon,
+        });
+        return epsonse.status(200).json({
+            user:{
+                id: userId,
+                email: user.email,
+                profileSetup: user.profileSetup
+            }
+        })
+    } catch (error) {
+        console.log(error);
+        return response.status(500).send("Internal Server error");
+    }
+}
---
+++ Server/models/UserModel.js
@@ -0,0 +1,46 @@
+import { genSalt, hash } from "bcrypt";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type: String,
+        required: [true,"Email is required"],
+        unique: true
+    },
+    password:{
+        type: String,
+        required: [true,"Password is required"],
+        unique: true
+    },
+    firstName:{
+        type: String,
+        unique: false
+    },
+    lastName:{
+        type: String,
+        unique: false
+    },
+    image:{
+        type: String,
+        unique: false
+    },
+    color:{
+        type: Number,
+        unique: false
+    },
+    profileSetup:{
+        type: Boolean,
+        unique: false
+    }
+})
+
+userSchema.pre("save", async function(next){
+    const salt = await genSalt();
+    this.password = await hash(this.password, salt)
+    next();
+})
+
+const User = mongoose.model("User", userSchema);
+
+
+export default User;
