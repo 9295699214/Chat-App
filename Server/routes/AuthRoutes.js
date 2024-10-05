8a70f2ecd601e675d58ce1291d71620d0109e67e
aakash0495 <aakash.chopra95@gmail.com>
2024-10-04 22:20:33 -0400

    Addd rount for update profile and code in controller

--- Server/routes/AuthRoutes.js
+++ Server/routes/AuthRoutes.js
@@ -6,5 +6,6 @@ const authRoutes= Router();
 authRoutes.post('/signup', signup);
 authRoutes.post('/login', login);
 authRoutes.get('/user-info', verifyToken, getUserInfo)
+authRoutes.post('/update-profile', verifyToken, updateProfile)
 
 export default authRoutes;
